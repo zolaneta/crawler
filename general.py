2942e7044bbd5caf3f79ff3c28727371723e097c
aneta <zolaneta@yahoo.com>
2016-03-28 20:55:20 -0400

    Added virtual env

--- general.py
+++ general.py
@@ -1,12 +1,14 @@
 import os
 
 
+# Each website you crawl is a separate project (folder)
 def create_project_dir(directory):
     if not os.path.exists(directory):
         print "creating directory " + directory
         os.makedirs(directory)
 
 
+# Create queue and crawled files (if not created)
 def create_data_files(project_name, base_url):
     queue = project_name + '/queue.txt'
     crawled = project_name + '/crawled.txt'
@@ -16,22 +18,26 @@ def create_data_files(project_name, base_url):
         write_file(crawled, '')
 
 
+# Create a new file
 def write_file(path, data):
     f = open(path, 'w')
     f.write(data)
     f.close()
 
 
+# Add data onto an existing file
 def append_to_file(path, data):
     with open(path, 'a') as f:
         f.write(data + '\n')
 
 
+# Delete the contents of a file
 def delete_file_contents(path):
     with open(path, 'w'):
         pass
 
 
+# Read a file and convert each line to set item
 def file_to_set(file_name):
     results = set()
     with open(file_name, 'rt') as f:
@@ -40,6 +46,7 @@ def file_to_set(file_name):
     return results
 
 
+# Iterate through a set, each item will be a new line in the file
 def set_to_file(links):  #sets
     delete_file_contents(file)
     for link in sorted(links):
